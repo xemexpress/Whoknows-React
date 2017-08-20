afce7cd97d5b849a756e88e1c36791b2b6594c32
Jason Luo <jasonl960523@gmail.com>
2017-08-20 16:45:45 +0800

    Ready for deploy

--- src/constants/general.js
+++ src/constants/general.js
@@ -1,8 +1,8 @@
-export const API_ROOT = 'http://localhost:3000/api'
-//export const API_ROOT = 'https://safe-crag-15458.herokuapp.com/api'
+// export const API_ROOT = 'http://localhost:3000/api'
+export const API_ROOT = 'https://safe-crag-15458.herokuapp.com/api'
 
-export const ADMIN_LIST = ['testtest']
-// export const ADMIN_LIST = ['whoknows']
+// export const ADMIN_LIST = ['testtest']
+export const ADMIN_LIST = ['whoknows']
 
 
 export const APP_NAME = 'Who knows'
